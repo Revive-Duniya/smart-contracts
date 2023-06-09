938559215bb16ca959508370b751f7a7dfe5e3f0
Jazthedev <joelpepa2@gmail.com>
2023-06-09 17:35:49 +0200

    demo nft subscriptions contract

---
+++ contract-susbcriptions/contracts/Subscriptions copy.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/HederaResponseCodes.sol";
+import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/IHederaTokenService.sol";
+import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/HederaTokenService.sol";
+import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/ExpiryHelper.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract Subscriptions is ExpiryHelper {
+    struct User {
+        address userAddr;
+        uint256 renewTimestamp;
+        uint256 subscriberLastPaid;
+    }
+    mapping(address => uint256) public balanceOf;
+    address public owner;
+    address public NftAddress;
+    uint256 public suscriptionAmount;
+    uint256 public tokenIdNumber;
+    uint256 creatednft;
+    mapping(address => User) public userData;
+
+    constructor(uint256 _suscriptionAmount) {
+        suscriptionAmount = _suscriptionAmount;
+        owner = msg.sender;
+    }
+
+    function initialCreateNFT() public {
+        require(creatednft == 0);
+        IHederaTokenService.TokenKey[]
+            memory keys = new IHederaTokenService.TokenKey[](1);
+
+        // Set this contract as supply
+        keys[0] = getSingleKey(
+            KeyType.SUPPLY,
+            KeyValueType.CONTRACT_ID,
+            address(this)
+        );
+
+        IHederaTokenService.HederaToken memory token;
+        token.name = "REVIVE DUNIYA";
+        token.symbol = "RDY";
+        token.memo = "memo";
+        token.treasury = address(this);
+        token.tokenSupplyType = true; // set supply to FINITE
+        token.maxSupply = 100000000;
+        token.tokenKeys = keys;
+        token.freezeDefault = false;
+        token.expiry = createAutoRenewExpiry(address(this), 7000000);
+
+        (int256 responseCode, address createdToken) = HederaTokenService
+            .createNonFungibleToken(token);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert("Failed to create non-fungible token");
+        }
+
+        NftAddress = createdToken;
+        creatednft += 1;
+    }
+
+    function userSubscribe() public payable {
+        require(msg.value > suscriptionAmount);
+        //if user does not own NFT
+        if (balanceOf[msg.sender] == 0) {
+            //mint nft
+            tokenIdNumber = tokenIdNumber + 1;
+            // _safeMint(msg.sender, tokenIdNumber);
+            // _setTokenURI(
+            //     tokenIdNumber,
+            //     generateMetadata(
+            //         tokenIdNumber,
+            //         1,
+            //         "subscription",
+            //         "ipfs://bafyreie3ichmqul4xa7e6xcy34tylbuq2vf3gnjf7c55trg3b6xyjr4bku/metadata.json"
+            //     )
+            // );
+            (int256 response, , int64[] memory serial) = HederaTokenService
+                .mintToken(
+                    NftAddress,
+                    0,
+                    generateMetadata(
+                        tokenIdNumber,
+                        1,
+                        "subscription",
+                        "ipfs://bafyreie3ichmqul4xa7e6xcy34tylbuq2vf3gnjf7c55trg3b6xyjr4bku/metadata.json"
+                    )
+                );
+
+            if (response != HederaResponseCodes.SUCCESS) {
+                revert("Failed to mint non-fungible token");
+            }
+            int256 response2 = HederaTokenService.transferNFT(
+                NftAddress,
+                address(this),
+                msg.sender,
+                serial[0]
+            );
+
+            if (response2 != HederaResponseCodes.SUCCESS) {
+                revert("Failed to transfer non-fungible token");
+            }
+            //renew subscriptions
+            userData[msg.sender] = User(
+                msg.sender,
+                block.timestamp + 30 * 24 * 60 * 60,
+                block.timestamp
+            );
+        }
+        //if user owns NFT, check if they have paid their subscription, if not pay and update due date and subscription status, also tier upgrade option
+        else {
+            //renew subscriptions
+            userData[msg.sender] = User(
+                msg.sender,
+                block.timestamp + 30 * 24 * 60 * 60,
+                block.timestamp
+            );
+        }
+        suscriptionAmount += msg.value;
+        balanceOf[msg.sender] += 1;
+    }
+
+    function generateMetadata(
+        uint256 tokenId,
+        uint256 assetId,
+        string memory name,
+        string memory ipfsimage
+    ) private pure returns (bytes[] memory) {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        name,
+                        Strings.toString(tokenId),
+                        '","assetid":"',
+                        Strings.toString(assetId),
+                        '", "description": "suscription of revive duniya game", "image": "data:image/svg+xml;base64,',
+                        ipfsimage,
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory metadata = string(json);
+
+        bytes[] memory metadataBytes = new bytes[](1);
+        metadataBytes[0] = bytes(metadata); // Convert string to bytes array
+
+        return metadataBytes;
+    }
+
+    function isUserSuscribed(address _user)
+        public
+        view
+        returns (bool, uint256)
+    {
+        return (
+            userData[_user].renewTimestamp > block.timestamp,
+            userData[_user].renewTimestamp
+        );
+    }
+
+    function setSubscriptionAmount(uint256 _amount) public {
+        require(msg.sender == owner);
+        suscriptionAmount = _amount;
+    }
+
+    function withdrawmoney(address payable _receiver) public {
+        require(msg.sender == owner);
+        suscriptionAmount = 0;
+        _receiver.transfer(suscriptionAmount);
+    }
+
+    function withdrawspecificamount(address payable _receiver, uint256 amount)
+        public
+    {
+        require(msg.sender == owner);
+        _receiver.transfer(amount);
+        suscriptionAmount -= amount;
+    }
+}
