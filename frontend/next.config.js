966c80397438b05945a931558dc91c5d03831853
Olumide Micheal <olumidemicheal7@gmail.com>
2023-06-14 17:20:51 +0100

    added config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['wallet.hashpack.app'],
-    },
-}
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['hashconnect']);
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    domains: ['wallet.hashpack.app'],
+  },
+  // Other Next.js configuration options
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = withPlugins([withTM], nextConfig);
