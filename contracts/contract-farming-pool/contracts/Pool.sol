ec9fe3d10ec959d0190ca74e8fd4075cb5e2b844
Jazthedev <joelpepa2@gmail.com>
2023-06-18 15:08:40 +0200

    fix pool contract

--- contracts/contract-farming-pool/contracts/Pool.sol
+++ contracts/contract-farming-pool/contracts/Pool.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
- import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/HederaTokenService.sol";
+//import "https://github.com/hashgraph/hedera-smart-contracts/blob/v0.2.0/contracts/hts-precompile/HederaTokenService.sol";
 
-//import "./HederaTokenService.sol";
+import "./HederaTokenService.sol";
 
 //farming
 contract Pool is HederaTokenService {
@@ -12,15 +12,15 @@ contract Pool is HederaTokenService {
         uint lockedRewards;//when user deposit tokens when he already have tokens, the timestamp is reseted and the rewards of the previous deposit gets stored here
     }
     address public owner;
-    address public lendedToken;
+    address public stakedToken;
     address public rewardedToken;
-    uint public lendedTokenAmount;
+    uint public stakedTokenAmount;
     uint public rewardedTokenAmount;
     mapping(address=>User) public userData;
     uint public apy;
 
-    constructor(address _lendedToken, address _rewardedToken, uint _apy){
-        int responseAssociation = HederaTokenService.associateToken(address(this),_lendedToken);
+    constructor(address _stakedToken, address _rewardedToken, uint _apy){
+        int responseAssociation = HederaTokenService.associateToken(address(this),_stakedToken);
             if (responseAssociation != HederaResponseCodes.SUCCESS) {
                 revert("Association lending Failed");
         }
@@ -29,7 +29,7 @@ contract Pool is HederaTokenService {
                 revert("Association reward Failed");
         }
         apy = _apy;
-        lendedToken = _lendedToken;
+        stakedToken = _stakedToken;
         rewardedToken = _rewardedToken;
         owner = msg.sender;
     }
@@ -42,7 +42,7 @@ contract Pool is HederaTokenService {
     function depositLendTokens(uint _amount)public{
         //transfer tokens
         int responseTransfer = HederaTokenService.transferToken(
-            lendedToken,
+            stakedToken,
             msg.sender,
             address(this),
             int64(int(_amount))
@@ -57,7 +57,7 @@ contract Pool is HederaTokenService {
         //update user data
         userData[msg.sender] = User(userData[msg.sender].lendedAmount+_amount,block.timestamp,accumulatedRewards+rewards);
         //update total amount 
-        lendedTokenAmount += _amount;
+        stakedTokenAmount += _amount;
     }
 
     function calculateRewards(uint _amountLended,uint startTimestamp) private view returns (uint){
@@ -74,10 +74,10 @@ contract Pool is HederaTokenService {
         return calculateRewards(userData[_user].lendedAmount,userData[_user].startTimestamp) + userData[_user].lockedRewards;
     }
 
-    function withdrawLendedTokens()public{
+    function withdrawstakedTokens()public{
         //transfer tokens
         int responseTransfer = HederaTokenService.transferToken(
-            lendedToken,            
+            stakedToken,            
             address(this),
             msg.sender,
             int64(int(userData[msg.sender].lendedAmount))
