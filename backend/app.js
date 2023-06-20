db932cb30baed5f8c727417208de54b725f6a1a6
Jazthedev <joelpepa2@gmail.com>
2023-06-20 16:24:49 +0200

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,20 @@ const cors = require('cors');
 // Instantiate an Express application
 const app = express();
 const dataRouter = require('./routers/dataRouter');
-app.use(cors());
-
+// app.use(cors());
+const corsOptions = {
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: '*'
+  };
+  
+app.use(cors(corsOptions));
 //parse json input with req.body
 app.use(express.json());
-
+app.all("*",(req,res,next)=>{
+    console.log("req");
+    next();
+})
 app.use("/api",dataRouter);
 
 app.all("*",(req,res,next)=>{
